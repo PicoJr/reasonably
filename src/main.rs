62a074dfe67a5875eacfc2571e32659c529cdb07
PicoJr <picojr_dev@gmx.com>
2024-10-30 21:59:34 +0100

    Add basic interns, relocate update logic to coroutine.

--- src/main.rs
+++ src/main.rs
@@ -28,29 +28,41 @@ fn App() -> Element {
 }
 
 #[component]
-fn DebugAction(mut bug_count: Signal<Decimal>) -> Element {
+fn DebugAction(mut logs: Signal<SimpleLogs>, mut debug_clicks: Signal<Decimal>) -> Element {
     rsx! {
-        button { onclick: move |_| bug_count -= Decimal::new(1.0), "Debug" }
+        button { onclick: move |_| {
+            debug_clicks += Decimal::new(1.0);
+            logs.write().log(
+                "debugging..."
+            )
+        }
+        , "Debug" }
     }
 }
 
 #[component]
-fn CodeAction(
-    mut logs: Signal<SimpleLogs>,
-    mut loc_count: Signal<Decimal>,
-    mut bug_count: Signal<Decimal>,
-) -> Element {
+fn CodeAction(mut logs: Signal<SimpleLogs>, mut code_clicks: Signal<Decimal>) -> Element {
     rsx! {
         button { onclick: move |_| {
-            let delta_loc = Decimal::new(1.0);
-            let delta_bug = Decimal::new(1.0);
-            loc_count += delta_loc;
-            bug_count += delta_bug;
+            code_clicks += Decimal::new(1.0);
             logs.write().log(
-                format!("coding...+{} loc +{} bugs", delta_loc, delta_bug).as_str()
+                "coding..."
             )
         }
         , "Code" }
+    }
+}
+
+#[component]
+fn HireInternAction(mut logs: Signal<SimpleLogs>, mut interns_clicks: Signal<Decimal>) -> Element {
+    rsx! {
+        button { onclick: move |_| {
+            interns_clicks += Decimal::new(1.0);
+            logs.write().log(
+                "hiring an intern..."
+            )
+        }
+        , "Hire interns" }
     }
 }
 
@@ -93,32 +105,76 @@ fn Logs(logs: Signal<SimpleLogs>) -> Element {
 #[component]
 fn Home() -> Element {
     let logs: Signal<SimpleLogs> = use_signal(SimpleLogs::new);
-    let mut loc_count: Signal<Decimal> = use_signal(|| Decimal::ZERO);
-    let bug_count: Signal<Decimal> = use_signal(|| Decimal::ZERO);
+
+    // clicks
+    let mut code_clicks: Signal<Decimal> = use_signal(|| Decimal::ZERO);
+    let mut debug_clicks: Signal<Decimal> = use_signal(|| Decimal::ZERO);
+    let mut interns_clicks: Signal<Decimal> = use_signal(|| Decimal::ZERO);
+
+    // production per clicks
+    let loc_per_clicks: Signal<Decimal> = use_signal(|| Decimal::new(1.0));
+    let debug_per_clicks: Signal<Decimal> = use_signal(|| Decimal::new(1.0));
+
+    let manual_bugs_ratio: Signal<Decimal> = use_signal(|| Decimal::new(1.0));
+    let interns_bugs_ratio: Signal<Decimal> = use_signal(|| Decimal::new(2.0));
+
+    // resources
+    let mut loc: Signal<Decimal> = use_signal(|| Decimal::ZERO);
+    let mut bugs: Signal<Decimal> = use_signal(|| Decimal::ZERO);
+
+    // producers
+    let mut interns: Signal<Decimal> = use_signal(|| Decimal::ZERO);
+    let interns_loc_dt: Signal<Decimal> = use_signal(|| Decimal::new(1.0));
 
     use_future(move || async move {
         loop {
-            sleep(std::time::Duration::from_millis(1000)).await;
-            loc_count += Decimal::new(1.0);
+            let dt = Decimal::new(0.01);
+            let manual_loc = code_clicks() * loc_per_clicks();
+            let manual_interns = interns_clicks();
+            let manual_bugs =
+                manual_loc * manual_bugs_ratio() - debug_clicks() * debug_per_clicks();
+
+            let auto_loc = interns() * interns_loc_dt() * dt;
+            let auto_bugs = interns() * interns_bugs_ratio() * dt;
+
+            loc += manual_loc + auto_loc;
+            bugs += manual_bugs + auto_bugs;
+            interns += manual_interns;
+
+            // reset clicks
+            *code_clicks.write() = Decimal::ZERO;
+            *debug_clicks.write() = Decimal::ZERO;
+            *interns_clicks.write() = Decimal::ZERO;
+            // sleep before next tick
+            sleep(std::time::Duration::from_millis(100)).await;
         }
     });
 
     rsx! {
         Logs {logs}
         div {
-            if loc_count() > Decimal::ZERO {
-                h1 {"Lines of code {loc_count().floor()}"}
+            if loc() > Decimal::ZERO {
+                h1 {"Lines of code {loc().floor()}"}
             }
-            if bug_count() > Decimal::ZERO {
-                h1 {"Bugs {bug_count().floor()}"}
+            if bugs() > Decimal::ZERO {
+                h1 {"Bugs {bugs().floor()}"}
+            }
+            if interns() > Decimal::ZERO {
+                h1 {"Interns {interns().floor()}"}
             }
             CodeAction{
                 logs,
-                loc_count,
-                bug_count,
+                code_clicks,
             }
-            if bug_count() > Decimal::ZERO {
-                DebugAction {bug_count}
+            HireInternAction {
+                logs,
+                interns_clicks,
+            }
+            if bugs() > Decimal::ZERO {
+                DebugAction {
+                    logs,
+                    debug_clicks,
+                }
             }
         }
     }
