67aa68755d212195721a99d43f6dc84d0aca972f
PicoJr <picojr_dev@gmx.com>
2024-11-10 23:33:08 +0100

    More verbose resources component

--- src/resources.rs
+++ src/resources.rs
@@ -4,7 +4,7 @@ use dioxus::core_macro::{component, rsx};
 use dioxus::dioxus_core::Element;
 use dioxus::prelude::{Signal};
 use dioxus::prelude::*;
-use crate::format_decimal::{format_decimal_loc, format_decimal_bugs, format_decimal_devs, format_decimal_hrs, format_decimal_pms};
+use crate::format_decimal::{format_decimal_loc, format_decimal_bugs, format_decimal_devs, format_decimal_hrs, format_decimal_pms, format_decimal_features};
 use crate::state::State;
 
 #[component]
@@ -44,30 +44,66 @@ pub(crate) fn Resources(
                         }
                     }
                 }
-                if (state.read().interns + state.read().manual_interns) > Decimal::ZERO {
+                if state.read().features > Decimal::ZERO {
+                    tr {
+                        td{"Features"}
+                        td{
+                            class: "table-value",
+                            "{format_decimal_features(state.read().features)}"
+                        }
+                    }
+                }
+                if state.read().interns > Decimal::ZERO {
+                    tr {
+                        td {"Interns (auto)"}
+                        td {
+                            class: "table-value",
+                            "{format_decimal_devs(state.read().interns)}"
+                        }
+                    }
+                }
+                if state.read().manual_interns > Decimal::ZERO {
                     tr {
                         td {"Interns"}
                         td {
                             class: "table-value",
-                            "{format_decimal_devs(state.read().interns + state.read().manual_interns)}"
+                            "{format_decimal_devs(state.read().manual_interns)}"
                         }
                     }
                 }
-                if (state.read().junior_devs + state.read().manual_junior_devs) > Decimal::ZERO {
+                if state.read().junior_devs > Decimal::ZERO {
+                    tr {
+                        td {"Junior devs (auto)"}
+                        td {
+                            class: "table-value",
+                            "{format_decimal_devs(state.read().junior_devs)}"
+                        }
+                    }
+                }
+                if state.read().manual_junior_devs > Decimal::ZERO {
                     tr {
                         td {"Junior devs"}
                         td {
                             class: "table-value",
-                            "{format_decimal_devs(state.read().junior_devs + state.read().manual_junior_devs)}"
+                            "{format_decimal_devs(state.read().manual_junior_devs)}"
                         }
                     }
                 }
-                if (state.read().senior_devs + state.read().manual_senior_devs) > Decimal::ZERO {
+                if state.read().senior_devs > Decimal::ZERO {
+                    tr {
+                        td{"Senior devs (auto)"}
+                        td{
+                            class: "table-value",
+                            "{format_decimal_devs(state.read().senior_devs)}"
+                        }
+                    }
+                }
+                if state.read().manual_senior_devs > Decimal::ZERO {
                     tr {
                         td{"Senior devs"}
                         td{
                             class: "table-value",
-                            "{format_decimal_devs(state.read().senior_devs + state.read().manual_senior_devs)}"
+                            "{format_decimal_devs(state.read().manual_senior_devs)}"
                         }
                     }
                 }
@@ -80,21 +116,39 @@ pub(crate) fn Resources(
                         }
                     }
                 }
-                if (state.read().hrs + state.read().manual_hrs) > Decimal::ZERO {
+                if state.read().hrs > Decimal::ZERO {
+                    tr {
+                        td{"HRs (auto)"}
+                        td{
+                            class: "table-value",
+                            "{format_decimal_hrs(state.read().hrs)}"
+                        }
+                    }
+                }
+                if state.read().manual_hrs > Decimal::ZERO {
                     tr {
                         td{"HRs"}
                         td{
                             class: "table-value",
-                            "{format_decimal_hrs(state.read().hrs + state.read().manual_hrs)}"
+                            "{format_decimal_hrs(state.read().manual_hrs)}"
                         }
                     }
                 }
-                if (state.read().pms + state.read().manual_pms) > Decimal::ZERO {
+                if state.read().pms > Decimal::ZERO {
+                    tr {
+                        td{"PMs (auto)"}
+                        td{
+                            class: "table-value",
+                            "{format_decimal_pms(state.read().pms)}"
+                        }
+                    }
+                }
+                if state.read().manual_pms > Decimal::ZERO {
                     tr {
                         td{"PMs"}
                         td{
                             class: "table-value",
-                            "{format_decimal_pms(state.read().pms + state.read().manual_pms)}"
+                            "{format_decimal_pms(state.read().manual_pms)}"
                         }
                     }
                 }
